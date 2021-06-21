4a43e1cb9f7cdc45711e3955e3bec1329e766318
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-06-21 12:20:29 +0200

    Add files via upload
    less than 30 sec

--- bellman-fort.py
+++ bellman-fort.py
@@ -1,77 +1,96 @@
-import pandas as pd
+import csv
+
+# CONSTANTE
+BUDGET_INITIAL = 500
+COMA = pow(10, 2)
+BENEFICE_REEL = 1000000
+DATA = 'brut2.csv'
 
 
-def purge(data_frame, price, profit):
-    for i, row in data_frame.iterrows():
-        if row[price] <= 0:
-            data_frame.drop(i, inplace=True)
-        if row[profit] <= 0:
-            data_frame.drop(i, inplace=True)
-    return data_frame
+def get_dataset(fichier):
+    """
+    :return:liste de liste contenant le nom de l'action, le prix et le bénéfice
+    """
+    dataset = []
+    with open(fichier, newline='') as data:
+        for row in csv.reader(data, delimiter=' '):
+            dataset.append(row[0].split(','))
+    return dataset
 
 
-def init(data_frame, max_budget):
-    for initialisation in range(max_budget):
-        data_frame[initialisation] = 0
+def purgedataset(dataset):
+    """
+    :param dataset: a list of list
+    :return: a dataset without incorrect value
+    """
+    del dataset[0]
+    i = 0
+    for row in range(len(dataset)):
+        try:
+            if float(dataset[i][1]) <= 0 or float(dataset[i][2]) <= 0:
+                del dataset[i]
+                i -= 1
+        except ValueError:
+            del dataset[i]
+        i += 1
+    return dataset
 
 
-def get_dimension_y(data_frame):
-    dimension = data_frame.shape
-    return dimension[0]
+def endofcoma(dataset):
+    """
+    :param dataset:  a list of list
+    :return: a dataset without coma and with rawbenefit
+    """
+    for row in dataset:
+        row[1] = int(float(row[1]) * COMA)
+        row[2] = int(float(row[2]) * COMA) * row[1]
+    return dataset
 
 
-def maximum(valeur1, valeur2):
-    if valeur1 > valeur2:
-        return valeur1
-    else:
-        return valeur2
-
-
-def matrice_bellman_fort(ant, dataframe, budget_max):
-    y = 0
-    for i in dataframe.iterrows():
-        poids_item = i[1]["price"]
-        valeur_item = i[1]["profit"]
-        for c in range(0, budget_max + 1):
-            realc = c + 3
-            if poids_item <= c:
-                dataframe.iloc[y, realc] = max(dataframe.iloc[y - 1, realc - poids_item] + valeur_item,
-                                               dataframe.iloc[y - 1, realc])
-                if dataframe.iloc[y - 1, realc - poids_item] + valeur_item >= dataframe.iloc[y, realc]:
-                    ant.iloc[y, realc] = realc - poids_item
-                else:
-                    ant.iloc[y, realc] = realc
+def bellmanford(budget, dataset):
+    """
+    :param budget: budget maximal du client
+    :param dataset: liste de liste contenant le nom de l'action, le prix et le bénéfice
+    :return: la liste des actions à acheté
+    """
+    matricebellmanford = [[0 for _ in range(budget + 1)] for _ in range(len(dataset) + 1)]
+    for i in range(1, len(dataset) + 1):
+        for prix in range(1, budget + 1):
+            if dataset[i - 1][1] <= prix:
+                matricebellmanford[i][prix] =\
+                    max(dataset[i - 1][2] + matricebellmanford[i - 1][prix - dataset[i - 1][1]],
+                        matricebellmanford[i - 1][prix])
             else:
-                ant.iloc[y, realc] = realc
-                dataframe.iloc[y, realc] = dataframe.iloc[y - 1, realc]
-        y += 1
+                matricebellmanford[i][prix] = matricebellmanford[i - 1][prix]
+    prix = budget
+    nbelement = len(dataset)
+    elements_selectionne = []
+    while prix >= 0 and nbelement >= 0:
+        action = dataset[nbelement - 1]
+        if matricebellmanford[nbelement][prix] == matricebellmanford[nbelement - 1][prix - action[1]] + action[2]:
+            elements_selectionne.append(action)
+            prix -= action[1]
+        nbelement -= 1
+    return matricebellmanford[-1][-1], elements_selectionne
 
 
-def get_resultat(ant, max_budget):
-    number_of_the_next_collum = max_budget + 3
-    number_of_the_ant_collum = number_of_the_next_collum
-    poids = 0
-    valeur = 0
-    souldbuy = ""
-    for i in range(nb_item - 1, -1, -1):
-        number_of_the_next_collum = ant.iloc[i, number_of_the_next_collum]
-        if not (number_of_the_next_collum == number_of_the_ant_collum):
-            souldbuy += f"{ant.iloc[i, 0]} / "
-            poids += ant.iloc[i, 1]
-            valeur += ant.iloc[i, 2]
-        number_of_the_ant_collum = number_of_the_next_collum
-    return souldbuy, poids, valeur,
+def showresulats(solution):
+    prix = 0
+    rawbenefit = solution[0]/BENEFICE_REEL
+    print("Vous devez acheter les actions suivantes")
+    for action in solution[1]:
+        print(action[0])
+        prix += action[1]
+    print(f"Prix Total : {round(prix/100, 2)} \n"
+          f"Bénéfice sur 2 ans :{round(rawbenefit, 2)}")
 
 
-budget = 500
-dataset = purge(pd.read_csv('brut.csv'), "price", "profit")
-dataset["profit"] = dataset["profit"]*dataset["price"]*0.01
-nb_item = get_dimension_y(dataset)
-init(dataset, budget+1)
-predecesseur = dataset.copy()
-matrice_bellman_fort(predecesseur, dataset, budget)
-result = get_resultat(predecesseur, budget)
-print(f"vous devez acheter les actions suivantes : {result[0]}")
-print(f"budget dépensé {result[1]}€")
-print(f"bénéfice {result[2]}€")
+def main():
+    dataset = get_dataset(DATA)
+    dataset = purgedataset(dataset)
+    dataset = endofcoma(dataset)
+    resultat = bellmanford(BUDGET_INITIAL * COMA, dataset)
+    showresulats(resultat)
 
+
+main()
